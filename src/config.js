3869914b87357c43d87d6235e0d917d32a4478e8
MJ <mkjain7428@gmail.com>
2025-08-03 13:39:17 +0530

    Update code so netlify can run api from unsecure end

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
-const API_BASE_URL = 'http://13.233.68.227';
+// const API_BASE_URL = 'http://13.233.68.227';
+
+// export { API_BASE_URL };
+
+const API_BASE_URL = '/api';
 
 export { API_BASE_URL };
